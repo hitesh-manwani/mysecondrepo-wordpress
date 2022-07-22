6331abb09fb69c39cada27210b7906dcbe3cab64
hitesh-manwani <109348270+hitesh-manwani@users.noreply.github.com>
2022-07-22 11:54:18 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-apt update -y 
-apt install nginx -y
-systemctl enable nginx
-systemctl start nginx
-apt install php php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+#apt update -y 
+#apt install nginx -y
+#systemctl enable nginx
+#systemctl start nginx
+#apt install php php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+echo "installed"
